a46d918cfe142c391f6f63e60a95776e84da4710
HuoHuas001 <2351078777@qq.com>
2023-07-05 01:56:30 +0800

    update

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include <llapi/EventAPI.h>
 #include <llapi/LoggerAPI.h>
 #include <llapi/mc/Level.hpp>
@@ -7,35 +6,10 @@
 #include "Version.h"
 #include <llapi/LLAPI.h>
 #include <llapi/HookAPI.h>
-
-Logger logger(PLUGIN_NAME);
-
-// #include <mc/LevelChunk.hpp>
-// #include <mc/Dimension.hpp>
-// #include <mc/ChunkSource.hpp>
-// #include <mc/DBChunkStorage.hpp>
-// #include <mc/ChunkPos.hpp>
-//
-// THook(void, "?loadChunk@DBChunkStorage@@UEAAXAEAVLevelChunk@@_N@Z", DBChunkStorage* _this, LevelChunk* a2, bool a3) {
-//     auto& pos = a2->getPosition();
-//     logger.warn("DBChunkStorage::loadChunk : LevelChunkPos: {}, {}; bool: {}", pos.x, pos.z, a3);
-//     return original(_this, a2, a3);
-// }
-//
-// THook(__int64, "?createNewChunk@ChunkSource@@UEAA?AV?$shared_ptr@VLevelChunk@@@std@@AEBVChunkPos@@W4LoadMode@1@_N@Z", __int64 a1, __int64* a2, __int64* a3, int a4, unsigned __int8 a5) {
-//     logger.warn("ChunkSource::createNewChunk : ChunkPos: {}, {}; LoadMode: {}; bool: {}", *a2, *a3, a4, a5);
-//     return original(a1, a2, a3, a4, a5);
-// }
-//
-// THook(UINT64, "?getOrLoadChunk@ChunkSource@@UEAA?AV?$shared_ptr@VLevelChunk@@@std@@AEBVChunkPos@@W4LoadMode@1@_N@Z", ChunkSource* _this,
-//     UINT64* a2,
-//     ChunkPos* a3,
-//     unsigned int a4,
-//     char a5) {
-//     logger.warn("ChunkSource::getOrLoadChunk : ChunkPos: {}, {}; LoadMode: {}; bool: {}", a3->x, a3->z, a4, a5);
-//     return original(_this, a2, a3, a4, a5);
-// }
-
+#include <llapi/RegCommandAPI.h>
+#include <llapi/mc/ItemStackRequestActionHandler.hpp>
+#include <llapi/mc/ItemStackRequestActionTransferBase.hpp>
+#include <llapi/mc/ItemStackRequestSlotInfo.hpp>
 #include <llapi/mc/BlockInstance.hpp>
 #include <llapi/mc/Block.hpp>
 #include <llapi/mc/BlockActor.hpp>
@@ -53,7 +27,10 @@ Logger logger(PLUGIN_NAME);
 #include <llapi/mc/ServerNetworkHandler.hpp>
 #include <llapi/mc/NetworkIdentifier.hpp>
 #include <llapi/mc/ContainerClosePacket.hpp>
-#include <llapi/RegCommandAPI.h>
+#include <llapi/ScheduleAPI.h>
+
+#define _QWORD unsigned __int64
+Logger logger(PLUGIN_NAME);
 
 std::unordered_map<Player *, BlockPos> RecordedInfo;
 
@@ -67,39 +44,87 @@ string addSoltTag(const string &str, int solt)
 void updateBlock(Player *pl, const BlockPos &pos)
 {
     pl->sendUpdateBlockPacket(pos, StaticVanillaBlocks::mChest->getRuntimeId());
+    pl->sendUpdateBlockPacket(pos.add(1, 0, 0), StaticVanillaBlocks::mChest->getRuntimeId());
     RecordedInfo[pl] = pos;
 }
 
 void updateBlockActor(Player *pl, const BlockPos &pos, const string &title, const std::unordered_map<int, string> &option)
 {
-    string chest = "{\"CustomName\":\"\",\"Findable\":0b,\"Items\":[";
+    string chest1 = "{\"CustomName\":\"\",\"Findable\":0b,\"Items\":[";
     if (!option.empty())
     {
-        chest += addSoltTag(option.begin()->second, option.begin()->first);
+        chest1 += addSoltTag(option.begin()->second, option.begin()->first);
         for (auto &it = ++option.begin(); it != option.end(); ++it)
         {
-            chest += ("," + addSoltTag(it->second, it->first));
+            if (it->first <= 26)
+            {
+                chest1 += ("," + addSoltTag(it->second, it->first));
+            }
         }
     }
-    chest += "],\"id\":\"Chest\",\"isMovable\":1b,\"x\":0,\"y\":0,\"z\":0}";
+    chest1 += "],\"id\":\"Chest\",\"isMovable\":1b,\"x\":0,\"y\":0,\"z\":0}";
 
-    auto nbt = CompoundTag::fromSNBT(chest);
+    string chest2 = "{\"CustomName\":\"\",\"Findable\":0b,\"Items\":[";
+    if (!option.empty())
+    {
+        bool first = true;
+        for (auto &it = ++option.begin(); it != option.end(); ++it)
+        {
+            if (it->first > 26)
+            {
+                if (first)
+                {
+                    chest2 += addSoltTag(it->second, it->first - 26);
+                    first = false;
+                }
+                else
+                {
+                    chest2 += ("," + addSoltTag(it->second, it->first - 26));
+                }
+            }
+        }
+    }
+    chest2 += "],\"id\":\"Chest\",\"isMovable\":1b,\"x\":0,\"y\":0,\"z\":0}";
 
-    nbt->putInt("x", pos.x);
-    nbt->putInt("y", pos.y);
-    nbt->putInt("z", pos.z);
-    nbt->putString("CustomName", title);
+    auto nbt1 = CompoundTag::fromSNBT(chest1);
+    nbt1->putInt("x", pos.x);
+    nbt1->putInt("y", pos.y);
+    nbt1->putInt("z", pos.z);
+    nbt1->putInt("pairx", pos.x + 1);
+    nbt1->putInt("pairz", pos.z);
+    nbt1->putByte("pairlead", 1);
+    nbt1->putString("CustomName", title);
 
-    BinaryStream bs;
-    bs.writeVarInt(pos.x);
-    bs.writeUnsignedVarInt(pos.y);
-    bs.writeVarInt(pos.z);
-    bs.writeCompoundTag(*nbt);
-    auto pkt = MinecraftPackets::createPacket(MinecraftPacketIds::BlockActorData);
-    pkt->read(bs);
-    pl->sendNetworkPacket(*pkt);
+    auto nbt2 = CompoundTag::fromSNBT(chest2);
+    nbt2->putInt("x", pos.x + 1);
+    nbt2->putInt("y", pos.y);
+    nbt2->putInt("z", pos.z);
+    nbt2->putInt("pairx", pos.x);
+    nbt2->putInt("pairz", pos.z);
+    nbt2->putByte("pairlead", 0);
+    nbt2->putString("CustomName", title);
+
+    // 放置箱子1
+    BinaryStream bs1;
+    bs1.writeVarInt(pos.x);
+    bs1.writeUnsignedVarInt(pos.y);
+    bs1.writeVarInt(pos.z);
+    bs1.writeCompoundTag(*nbt1);
+    auto pkt1 = MinecraftPackets::createPacket(MinecraftPacketIds::BlockActorData);
+    pkt1->read(bs1);
+    pl->sendNetworkPacket(*pkt1);
+    // 放置箱子2
+    BinaryStream bs2;
+    bs2.writeVarInt(pos.x + 1);
+    bs2.writeUnsignedVarInt(pos.y);
+    bs2.writeVarInt(pos.z);
+    bs2.writeCompoundTag(*nbt2);
+    auto pkt2 = MinecraftPackets::createPacket(MinecraftPacketIds::BlockActorData);
+    pkt2->read(bs2);
+    pl->sendNetworkPacket(*pkt2);
 }
 
+// 打开容器包
 void open(Player *pl, const BlockPos &pos)
 {
     BinaryStream bs;
@@ -114,24 +139,49 @@ void open(Player *pl, const BlockPos &pos)
     pl->sendNetworkPacket(*pkt);
 }
 
+// 打开箱子UI
 void OpenContainerUI(Player *pl, string title, std::unordered_map<int, string> option)
 {
+    // if(pl->getPlatform())
     auto pos = pl->getBlockPos();
+    pos = pos.add(0, 5, 0);
     updateBlock(pl, pos);
     updateBlockActor(pl, pos, title, option);
-    open(pl, pos);
+    Schedule::delay([pl, pos]
+                    { open(pl, pos); },
+                    0.1);
 }
 
+// 关闭箱子UI
+void CloseContainerUI(Player *pl)
+{
+    auto it = RecordedInfo.find(pl);
+    if (it != RecordedInfo.end())
+    {
+        pl->sendUpdateBlockPacket(it->second, pl->getBlockSource()->getBlock(it->second).getRuntimeId());
+        pl->sendUpdateBlockPacket(it->second.add(1, 0, 0), pl->getBlockSource()->getBlock(it->second).getRuntimeId());
+        RecordedInfo.erase(it);
+    }
+}
+
+// 注册命令
 class openCommand : public Command
 {
 public:
     void execute(CommandOrigin const &ori, CommandOutput &output) const override
     { // 执行部分
         ServerPlayer *sp = ori.getPlayer();
+        // 放入第一个箱子即为Slot显示的数字，第二个箱子即为26+Slot显示的数字(可以无需去掉Slot，会自己覆盖)
         std::unordered_map<int, string> v = {
-            {0, "{\"Block\":{\"name\":\"minecraft:enchanting_table\",\"states\":{},\"version\":18087969},\"Count\":64b,\"Damage\":0s,\"Name\":\"minecraft:enchanting_table\",\"WasPickedUp\":0b,\"tag\":{\"display\":{\"Lore\":[\"测试选项1\",\"测试2\",\"测3\"]}}}"},
-            {1, "{\"Block\":{\"name\":\"minecraft:enchanting_table\",\"states\":{},\"version\":18087969},\"Count\":64b,\"Damage\":0s,\"Name\":\"minecraft:enchanting_table\",\"WasPickedUp\":0b,\"tag\":{\"display\":{\"Lore\":[\"测1\",\"测2\",\"测3\"]}}}"},
-            {2, "{\"Block\":{\"name\":\"minecraft:enchanting_table\",\"states\":{},\"version\":18087969},\"Count\":64b,\"Damage\":0s,\"Name\":\"minecraft:enchanting_table\",\"WasPickedUp\":0b,\"tag\":{\"display\":{\"Lore\":[\"114514\",\"1919810\"]}}}"}};
+            {13, "{\"Count\":1b,\"Damage\":3s,\"Name\":\"minecraft:skull\",\"Slot\":13b,\"WasPickedUp\":0b,\"tag\":{\"RepairCost\":0,\"display\":{\"Name\":\"个人信息\"}}}"},
+            {19, "{\"Count\":1b,\"Damage\":0s,\"Name\":\"minecraft:diamond\",\"Slot\":19b,\"WasPickedUp\":0b,\"tag\":{\"RepairCost\":0,\"display\":{\"Name\":\"商店\"}}}"},
+            {21, "{\"Count\":1b,\"Damage\":0s,\"Name\":\"minecraft:wooden_door\",\"Slot\":21b,\"WasPickedUp\":0b,\"tag\":{\"RepairCost\":0,\"display\":{\"Name\":\"我的家园\"}}}"},
+            {23, "{\"Block\":{\"name\":\"minecraft:stained_glass\",\"states\":{\"color\":\"gray\"},\"version\":18087969},\"Count\":1b,\"Damage\":0s,\"Name\":\"minecraft:stained_glass\",\"Slot\":23b,\"WasPickedUp\":0b,\"tag\":{\"RepairCost\":0,\"display\":{\"Name\":\"工会菜单\"}}}"},
+            {25, "{\"Block\":{\"name\":\"minecraft:chest\",\"states\":{\"facing_direction\":0},\"version\":18087969},\"Count\":1b,\"Damage\":0s,\"Name\":\"minecraft:chest\",\"Slot\":25b,\"WasPickedUp\":0b,\"tag\":{\"RepairCost\":0,\"display\":{\"Name\":\"我的背包\"}}}"},
+            {37, "{\"Block\":{\"name\":\"minecraft:command_block\",\"states\":{\"conditional_bit\":0b,\"facing_direction\":0},\"version\":18087969},\"Count\":1b,\"Damage\":0s,\"Name\":\"minecraft:command_block\",\"Slot\":11b,\"WasPickedUp\":0b,\"tag\":{\"RepairCost\":0,\"display\":{\"Name\":\"管理菜单\"}}}"},
+            {39, "{\"Block\":{\"name\":\"minecraft:redstone_lamp\",\"states\":{},\"version\":18087969},\"Count\":1b,\"Damage\":0s,\"Name\":\"minecraft:redstone_lamp\",\"Slot\":13b,\"WasPickedUp\":0b,\"tag\":{\"RepairCost\":0,\"display\":{\"Name\":\"开关服务器\"}}}"},
+            {41, "{\"Block\":{\"name\":\"minecraft:enchanting_table\",\"states\":{},\"version\":18087969},\"Count\":1b,\"Damage\":0s,\"Name\":\"minecraft:enchanting_table\",\"WasPickedUp\":0b,\"tag\":{\"display\":{\"Lore\":[\"NMSL\"]}}}"},
+        };
         OpenContainerUI(sp, "Chest Form Test", v);
     }
 
@@ -148,21 +198,34 @@ void PluginInit()
                                   {
     openCommand::setup(ev.mCommandRegistry);
     return true; });
-
-    Event::ConsoleCmdEvent::subscribe([](const Event::ConsoleCmdEvent &ev)
-                                      { return true; });
 }
 
+// 关闭容器刷新方块
 THook(void, "?handle@ServerNetworkHandler@@UEAAXAEBVNetworkIdentifier@@AEBVContainerClosePacket@@@Z",
       ServerNetworkHandler *_this, NetworkIdentifier *a2, ContainerClosePacket *a3)
 {
     Player *pl = (Player *)_this->getServerPlayer(*a2, 0);
+    CloseContainerUI(pl);
+    return original(_this, a2, a3);
+}
+
+// Player选择回调
+THook(char, "?_handleTransfer@ItemStackRequestActionHandler@@AEAA?AW4ItemStackNetResult@@AEBVItemStackRequestActionTransferBase@@_N11@Z",
+      ItemStackRequestActionHandler *_this,
+      ItemStackRequestActionTransferBase *a2,
+      bool a3,
+      bool a4,
+      bool a5)
+{
+    Player *pl = ((Player *)(*(_QWORD *)_this));
     auto it = RecordedInfo.find(pl);
+    //回调，请自行完善功能
     if (it != RecordedInfo.end())
     {
-        pl->sendUpdateBlockPacket(it->second, pl->getBlockSource()->getBlock(it->second).getRuntimeId());
-        RecordedInfo.erase(it);
+        ItemStackRequestSlotInfo *modifier = const_cast<ItemStackRequestSlotInfo *>(&a2->getSrc());
+        int slot = *(((UINT8 *)modifier) + 1); //玩家选择的Slot
+        logger.info("{} Choose {}", pl->getName(), slot);
     }
 
-    return original(_this, a2, a3);
+    return original(_this, a2, a3, a4, a5);
 }
